b102bd05bd6b3bc5a8e0a6bed6a4af7e95c8c644
Laszlo-Lazuer <visualjazz@gmail.com>
2018-08-02 14:12:55 -0500

    refactor - rename App to AuthorQuiz.

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import './bootstrap.min.css';
+
+class AuthorQuiz extends Component {
+  render() {
+    return (
+      <div className="App">
+        AuthorQuiz
+      </div>
+    );
+  }
+}
+
+export default AuthorQuiz;
